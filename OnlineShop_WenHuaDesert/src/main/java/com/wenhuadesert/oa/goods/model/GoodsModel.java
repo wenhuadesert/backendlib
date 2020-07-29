225c06d8dda434ddeff499c80e7bdabd3cc4bdd0
wxp <weiaibeachsea@sina.com>
2020-07-29 18:54:21 +0800

    wxp修改goodsModel属性

--- OnlineShop_WenHuaDesert/src/main/java/com/wenhuadesert/oa/goods/model/GoodsModel.java
+++ OnlineShop_WenHuaDesert/src/main/java/com/wenhuadesert/oa/goods/model/GoodsModel.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import org.apache.ibatis.type.Alias;
 
-
 @Alias("Goods")
 public class GoodsModel implements Serializable {
 	private int goodsId = 0;//
@@ -12,7 +11,9 @@ public class GoodsModel implements Serializable {
 	private double price = 0;
 	private int goodsStock = 0;
 	private String brand = null;
-
+	private byte[] photo = null; // 商品照片
+	private String photoFileName = null; // 照片文件名
+	private String photoContentType = null; // 照片类型
 	private StorehouseModel storehouse = null;
 	private CategoryModel category = null;
 
@@ -70,6 +71,30 @@ public class GoodsModel implements Serializable {
 
 	public void setStorehouse(StorehouseModel storehouse) {
 		this.storehouse = storehouse;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public String getPhotoFileName() {
+		return photoFileName;
+	}
+
+	public void setPhotoFileName(String photoFileName) {
+		this.photoFileName = photoFileName;
+	}
+
+	public String getPhotoContentType() {
+		return photoContentType;
+	}
+
+	public void setPhotoContentType(String photoContentType) {
+		this.photoContentType = photoContentType;
 	}
 
 }
